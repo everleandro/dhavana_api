8a388a17d31d7764ea7f3e05f9cc043e0209887e
Ever <ever.leandros91@gmail.com>
2025-03-13 14:22:49 -0500

    Refactor settings.py to use environ for SECRET_KEY retrieval

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ import os
 import dj_database_url
 import environ
 from datetime import timedelta
-env=environ.Env()
+env = environ.Env()
 environ.Env.read_env()
 
 ENVIROMENT=env
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 
 if not SECRET_KEY:
